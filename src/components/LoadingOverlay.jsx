0a7a65f6dc31401da4813d8fd4eb204c1e2af692
jmarvillier <44295084+jmarvillier@users.noreply.github.com>
2026-02-23 23:31:59 +0100

    Delete src directory

--- src/components/LoadingOverlay.jsx
+++
@@ -1,9 +0,0 @@
-export default function LoadingOverlay({ visible, text }) {
-  return (
-    <div id="loadingOverlay" className={visible ? 'show' : ''}>
-      <div className="ld-logo">WEEX</div>
-      <div className="ld-bar"><div className="ld-fill" /></div>
-      <div className="ld-txt">{text || 'Chargement…'}</div>
-    </div>
-  )
-}
--- src/lib/config.js
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_SHEET_ID   = '1Cec11I3_NVlEedIQoBtomF2MzHBBaK0I'
-export const DEFAULT_SHEET_URL  = `https://docs.google.com/spreadsheets/d/${DEFAULT_SHEET_ID}/edit`
-export const DEFAULT_SHEET_NAME = 'Mon Journal WEEX'
