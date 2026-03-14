235164f33f00a94f7f2b0fd1b56a2c2cf2971340
jmarvillier <44295084+jmarvillier@users.noreply.github.com>
2026-03-14 19:29:02 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import './styles/pair-card.css'
-import { useState, useEffect }       from 'react'
+import { useState, useEffect }        from 'react'
 import { useTrading }                 from './hooks/useTrading.js'
 import LoadingOverlay                 from './components/LoadingOverlay.jsx'
 import Landing                        from './components/Landing.jsx'
