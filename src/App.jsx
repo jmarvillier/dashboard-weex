d8ee775069b2d96d2520107a95732f22a014e985
jmarvillier <44295084+jmarvillier@users.noreply.github.com>
2026-07-01 18:54:12 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ export default function App() {
 
   const {
     view, zone, loading, loadingTxt,
-    fileName, loadedAt, pairList, rawRows, excluded, driveErr,
+    fileName, loadedAt, pairList, rawRows, xperpTrades, excluded, driveErr,
     repoAvailable,
     prices, priceSources, pricesLoading, pricesError, priceSource, lastPriceUpdate, refreshPrices,
     setZone, setDriveErr,
@@ -92,6 +92,7 @@ export default function App() {
           excluded={excluded}
           pairList={pairList}
           rawRows={rawRows}
+          xperpTrades={xperpTrades}
           prices={prices}
           priceSources={priceSources}
           repoAvailable={repoAvailable}
